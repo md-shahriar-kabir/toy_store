86dbf25a7a53cfaa2f1ece61031c572cb84e6fee
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-07-15 12:45:02 +0600

    updated next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i.ibb.co.com"', // আপনার ইমেজের ডোমেইন নাম এখানে দিন
-        port: '',
-        pathname: '/**', // সব পাথের ইমেজ এলাউ করার জন্য
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com', // যদি গুগল অ্যাভাটার ব্যবহার করেন
+        hostname: 'i.ibb.co.com',
         port: '',
         pathname: '/**',
       },
@@ -20,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
